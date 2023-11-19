4d92e2129e97ca3c201dac6685d060a702e414f4
Daniel Plotnikov <daniilrew@gmail.com>
2023-11-19 16:23:12 +0500

    raw data and raw readme

--- generate_data.py
+++ generate_data.py
@@ -6,7 +6,7 @@ API_KEY = str(sets.params_apikey())
 symbols_from_settings = list(sets.params_currencies())
 
 
-def get_data_for_raw_layer(symbol=symbols_from_settings):
+def get_data_for_raw_layer(symbols=symbols_from_settings):
     """
     Генерирует данные для сырого слоя DWH за счет получения информации с API.
     """
@@ -16,33 +16,33 @@ def get_data_for_raw_layer(symbol=symbols_from_settings):
     else: pass
     total_list = []
     for symbol in symbols:
-        try:
-            access_link = f"https://www.alphavantage.co/query"
-            params = {
-                'function': 'TIME_SERIES_INTRADAY',
-                'symbol' : symbol,
-                'interval': '60min',
-                'outputsize': 'full',
-                'apikey': API_KEY
-            }
-            response = requests.get(access_link, params=params)
-            result = response.json()
-            datetimes_list = list(result['Time Series (60min)'].keys())
-            stats_list = [
-            [
-                datetime_list, 
-                result['Meta Data']['2. Symbol'],
-                result['Time Series (60min)'][datetime_list]['1. open'],
-                result['Time Series (60min)'][datetime_list]['2. high'],
-                result['Time Series (60min)'][datetime_list]['3. low'],
-                result['Time Series (60min)'][datetime_list]['4. close'],
-                result['Time Series (60min)'][datetime_list]['5. volume']
-            ] 
-            for datetime_list in datetimes_list]
-            total_list += stats_list
-        except Exception as e: 
-            print(e)
-            continue
+        # try:
+        access_link = f"https://www.alphavantage.co/query"
+        params = {
+            'function': 'TIME_SERIES_INTRADAY',
+            'symbol' : symbol,
+            'interval': '15min',
+            'outputsize': 'full',
+            'apikey': API_KEY
+        }
+        response = requests.get(access_link, params=params)
+        result = response.json()
+        datetimes_list = list(result['Time Series (15min)'].keys())
+        stats_list = [
+        [
+            datetime_list, 
+            result['Meta Data']['2. Symbol'],
+            result['Time Series (15min)'][datetime_list]['1. open'],
+            result['Time Series (15min)'][datetime_list]['2. high'],
+            result['Time Series (15min)'][datetime_list]['3. low'],
+            result['Time Series (15min)'][datetime_list]['4. close'],
+            result['Time Series (15min)'][datetime_list]['5. volume']
+        ] 
+        for datetime_list in datetimes_list]
+        total_list += stats_list
+        # except Exception as e: 
+        #     print(e)
+        #     continue
     return pd.DataFrame(data=total_list, columns=['date', 'currency', 'open', 'high', 'low', 'close', 'volume'])
 
 def get_data_for_core_layer():
--- project_settings.py
+++ project_settings.py
@@ -1,2 +1,2 @@
 def params_apikey(): return 'FILL_BY_YOUR_VALUE'
-def params_currencies(): return 'AAPL', 'GOOGL'
+def params_currencies(): return ['AAPL', 'GOOGL', 'AMD', 'IBM']
