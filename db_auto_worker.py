6b54e4503808508ab0fc328137ebe433b85a970c
Daniel Plotnikov <daniilrew@gmail.com>
2023-11-25 23:21:54 +0500

    modules reforming
    - Отказ от использования отдельного модуля для генерации данных
    - Написана функция для генерации данных для core слоя

--- db_auto_worker.py
+++ db_auto_worker.py
@@ -1,6 +1,7 @@
 from sqlalchemy import text, create_engine, DateTime, engine
 from sqlalchemy_utils import database_exists, create_database
 import pandas as pd
+import requests
 
 
 # file for Apache Airflow
@@ -22,6 +23,48 @@ def get_conn():
 
     return conn
 
+def get_data_for_raw_layer(API_KEY, symbols):
+    """
+    Генерирует данные для сырого слоя DWH за счет получения информации с API.
+    ---
+    ---
+    params:\n
+    API_KEY - API ключ для сайта alphavantage.co\n
+    symbols - названия курсов, для которых нужно получить данные\n
+    ---
+    output: \n
+    pd.DataFrame
+    """
+    if type(symbols) == str: symbols = [symbols]
+    else: pass
+    total_list = []
+    for symbol in symbols:
+        access_link = f"https://www.alphavantage.co/query"
+        # задаем параметры для работы с API
+        params = {
+            'function': 'TIME_SERIES_INTRADAY',
+            'symbol' : symbol,
+            'interval': '15min',
+            'outputsize': 'full',
+            'apikey': API_KEY
+        }
+        response = requests.get(access_link, params=params)
+        result = response.json()
+        datetimes_list = list(result['Time Series (15min)'].keys())
+        stats_list = [
+        [
+            datetime_list, 
+            result['Meta Data']['2. Symbol'],
+            result['Time Series (15min)'][datetime_list]['1. open'],
+            result['Time Series (15min)'][datetime_list]['2. high'],
+            result['Time Series (15min)'][datetime_list]['3. low'],
+            result['Time Series (15min)'][datetime_list]['4. close'],
+            result['Time Series (15min)'][datetime_list]['5. volume']
+        ] 
+        for datetime_list in datetimes_list]
+        total_list += stats_list
+    return pd.DataFrame(data=total_list, columns=['date', 'currency', 'open', 'high', 'low', 'close', 'volume'])
+
 def upload_data_to_raw_layer(raw_data: pd.DataFrame, conn:engine.base.Connection):
     try:
         dtype = {
@@ -39,3 +82,22 @@ def upload_data_to_raw_layer(raw_data: pd.DataFrame, conn:engine.base.Connection
         return True
     except Exception as e:
         return e
+
+def core_layer_data_worker(conn:engine.base.Connection):
+    data_from_db = pd.read_sql('SELECT * FROM raw_data;', conn)
+
+    # создаем "родительскую" таблицу для всех курсов
+    total_df = pd.DataFrame(columns=['cur_id', 'cur_name'], data=list(zip(range(len(data_from_db['currency'].unique())), data_from_db['currency'].unique())))
+    total_df.to_sql('core_currencies', conn, if_exists='replace', index=False)
+    conn.execute('ALTER TABLE core_currencies ADD PRIMARY KEY (cur_id);')
+
+    # генерируем таблицы с данными по отдельным курсам.
+    dtype = {"date": DateTime}
+    for cur_name in data_from_db['currency'].unique():
+        copy_df = data_from_db[data_from_db['currency']==cur_name].copy(deep=True)
+        copy_df = copy_df[['date', 'open', 'high', 'low', 'close', 'volume']]
+        copy_df['cur_id'] = [total_df[total_df['cur_name']==cur_name]['cur_id'].tolist()[0]]*len(copy_df)
+        copy_df['id'] = range(len(copy_df))
+        copy_df = copy_df[['id', 'cur_id', 'date', 'open', 'high', 'low', 'close', 'volume']]
+        copy_df.to_sql(f'core_{cur_name.lower()}_curdata', conn, if_exists='replace', index=False, dtype=dtype)
+        conn.execute(f'ALTER TABLE core_{cur_name}_curdata ADD CONSTRAINT cur_id FOREIGN KEY (cur_id) REFERENCES core_currencies (cur_id);')
--- project_settings.py
+++ project_settings.py
@@ -1,2 +1,2 @@
-def params_apikey(): return 'FILL_BY_YOUR_VALUE'
+def params_apikey(): return 'NONE'
 def params_currencies(): return ['AAPL', 'GOOGL', 'AMD', 'IBM']
