d1ade59c7f47e4a48564710b180d65cdbb7f08d2
Daniel Plotnikov <daniilrew@gmail.com>
2023-11-26 15:25:31 +0500

    создание dag

--- project_settings.py
+++ project_settings.py
@@ -1,2 +1,9 @@
 def params_apikey(): return 'NONE'
 def params_currencies(): return ['AAPL', 'GOOGL', 'AMD', 'IBM']
+
+def get_data_db(): return "postgresql+psycopg2"
+def get_data_login(): return "postgres"
+def get_data_password(): return "postgres"
+def get_data_ip(): return "localhost"
+def get_data_port(): return "5432"
+def get_data_name_bd(): return "currency_dwh"
